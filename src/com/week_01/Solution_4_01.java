aed3a9eb63cbb944a9ae00fc717671df10aa62e5
Khrolenko <artem.khrolenko@gmail.com>
2016-10-25 22:23:42 +0300

    Commit 25_10_16

---
+++ src/com/week_01/Solution_4_01.java
@@ -0,0 +1,10 @@
+package com.week_01;
+
+/**
+ * Created by Khrolenko on 25.10.2016.
+ */
+public class Solution_4_01 {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
