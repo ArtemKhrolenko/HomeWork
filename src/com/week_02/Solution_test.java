d4d63bc5d8aba1c6baa753a1bcf35e8b936c662c
Khrolenko <artem.khrolenko@gmail.com>
2016-10-25 22:26:47 +0300

    Commit 25_10_16

---
+++ src/com/week_02/Solution_test.java
@@ -0,0 +1,10 @@
+package com.week_02;
+
+/**
+ * Created by Khrolenko on 25.10.2016.
+ */
+public class Solution_test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
