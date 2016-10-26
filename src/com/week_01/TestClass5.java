57d17bdd327ce16be5359d9bb1fd7c7df41e0f7d
Khrolenko <Artem.Khrolenko@gmail.com>
2016-10-26 09:05:21 +0300

    Commit Artem

---
+++ src/com/week_01/TestClass5.java
@@ -0,0 +1,10 @@
+package com.week_01;
+
+/**
+ * Created by Khrolenko on 26.10.2016.
+ */
+public class TestClass5 {
+    public static void main(String[] args) {
+        System.out.println("asdadadadTEst5");
+    }
+}
