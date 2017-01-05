7900b8af4c9ccf297c8f06add56b84ea55818d33
Khrolenko <Artem.Khrolenko@gmail.com>
2017-01-05 12:10:49 +0200

    Complete tasks week 02_Sol 4_1 -49

--- src/com/week_02/Solution_04_01.java
+++ src/com/week_02/Solution_04_01.java
@@ -1,11 +1,13 @@
 package com.week_02;
 
+import java.util.Arrays;
+
 /**
  * Найти минимальное и максимальное значения в массиве и вывести их на консоль
  */
 public class Solution_04_01 {
     public static void main(String[] args) {
-        int[] arr = new int[]{2, -1, 3, 3, 8, 2, 3};
+        int[] arr = ArrayUtils.generateMas(7, 54);
         int max = arr[0];
         int min = arr[0];
         for (int i = 1; i < arr.length; i++){
@@ -14,6 +16,7 @@ public class Solution_04_01 {
             if (arr[i] < min)
                 min = arr[i];
         }
+        System.out.println(Arrays.toString(arr));
         System.out.printf("Max: %d, Min: %d", max, min);
     }
 }
---
+++ src/com/week_02/Solution_04_03.java
@@ -0,0 +1,22 @@
+package com.week_02;
+
+import java.util.Arrays;
+
+/**
+ * Заданы два массива одинаковой длинны с любыми значениями скопировать данные из первого массива во второй
+ */
+public class Solution_04_03 {
+    public static void main(String[] args) {
+        int[] array_one = ArrayUtils.generateMas(7, 54);
+        int[] array_two = ArrayUtils.generateMas(7, 54);
+        /*for (int i = 0; i < array_one.length; i++){
+            array_two[i] = array_one[i];
+        }*/
+
+        //Либо - копирование одной строкой
+        array_two = Arrays.copyOf(array_one, array_one.length);
+
+        System.out.println("Оригинальный массив: \n" + Arrays.toString(array_one));
+        System.out.println("Измененный массив: \n"   + Arrays.toString(array_two));
+    }
+}
