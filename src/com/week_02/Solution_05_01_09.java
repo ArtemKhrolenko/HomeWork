e99e790c687bd25fa9f3fcb910f61ee16138d3f1
Khrolenko <artem.khrolenko@gmail.com>
2017-01-21 12:29:08 +0200

    Week_1 Complete

--- src/com/week_02/Solution_05_01_09.java
+++ src/com/week_02/Solution_05_01_09.java
@@ -1,5 +1,7 @@
 package com.week_02;
 
+import java.util.Arrays;
+
 /**
  * Return an array that is "left shifted" by one -- so {6, 2, 5, 3} returns {2, 5, 3, 6}.
  * You may modify and return the given array, or return a new array.
@@ -7,17 +9,16 @@ package com.week_02;
  */
 public class Solution_05_01_09 {
     public static void main(String[] args) {
-        int[] array = {6, 2, 5, 3};
-        int n = 2;
+        int[] array = {1};
+        int n = 1;
         int tmpVar;
-        for (int i = 0; i < n; i++){
+        for (int i = 1; i < n + 1; i++){
+            tmpVar = array[0];
             for (int j = 0; j < array.length - 1; j++){
-                tmpVar = array[j + 1];
-                array[j + 1] = array [j];
-                array[j] = array[array.length - 1];
+                array[j] = array [j + 1];
             }
+            array[array.length - 1] = tmpVar;
         }
-
-
+        System.out.println(Arrays.toString(array));
     }
 }
