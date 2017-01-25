9382387d9c336cf3809dd56cd87b8d34a2232883
Khrolenko <artem.khrolenko@gmail.com>
2017-01-25 23:39:02 +0200

    Week_1 Complete

---
+++ src/com/week_02/Solution_06_01.java
@@ -0,0 +1,18 @@
+package com.week_02;
+
+/**
+ * Given a string, return a string where for every char in the original, there are two chars.
+ */
+public class Solution_06_01 {
+    public static String doubleChar(String str1){
+        String resultStr = "";
+        for (char elem: str1.toCharArray()){
+            for (int i = 0; i < 2; i++)
+                resultStr += String.valueOf(elem);
+        }
+        return resultStr;
+    }
+    public static void main(String[] args) {
+        System.out.println(doubleChar("The"));
+    }
+}
